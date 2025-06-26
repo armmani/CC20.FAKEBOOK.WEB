04051663617e35c6554608b29944f7363ef9e58f
armmani <arm.maniii@gmail.com>
2025-06-26 11:37:50 +0700

    ทดสอบทำ routes แบบ 2

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -8,20 +8,31 @@ import {
 const guestRouter = createBrowserRouter([
   { path: "/", element: <p>LogIN</p> },
   { path: "/ads", element: <p>Ads</p> },
-  { path: "*", element: <p>NoWhere404</p> },
+  { path: "*", element: <Navigate to="/" replace /> },
 ]);
 
 const userRouter = createBrowserRouter([
-  { path: "/", element: <p>userHome</p> },
-  { path: "/friends", element: <p>friendPage</p> },
-  { path: "/profile", element: <p>profilePage</p> },
-  { path: "*", element: <p>404PageNotFoundUser</p> },
+  {
+    path: "/",
+    element: (
+      <div>
+        <div className="text-4xl py-4 border">Header</div>
+        <Outlet />
+      </div>
+    ),
+    children: [
+      { path: "", element: <p>userHomePage</p> },
+      { path: "friends", element: <p>friendPage</p> },
+      { path: "profile", element: <p>profilePage</p> },
+      { path: "*", element: <Navigate to="/" /> },
+    ],
+  },
 ]);
 
 function AppRouter() {
   return (
-  // <RouterProvider router={guestRouter} />
-  <RouterProvider router={userRouter} />
-)
+    // <RouterProvider router={guestRouter} />
+    <RouterProvider router={userRouter} />
+  );
 }
 export default AppRouter;
