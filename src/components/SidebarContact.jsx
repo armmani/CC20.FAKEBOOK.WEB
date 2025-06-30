9c1a8e34ac864832b33ef34106b41f166d22252b
armmani <arm.maniii@gmail.com>
2025-06-30 10:18:40 +0700

    CreatePost begin

--- src/components/SidebarContact.jsx
+++ src/components/SidebarContact.jsx
@@ -1,6 +1,36 @@
+import { SearchIcon } from "../icons";
+import Avatar from "./Avatar";
+import MenuItem from "./MenuItem";
+
 function SidebarContact() {
   return (
-    <div className="fixed top-14 right-0 h-full w-[350px] overflow-auto flex flex-col gap-2 bg-red-800">SidebarContact</div>
-  )
+    <div className="fixed top-14 right-0 h-full w-[350px] overflow-auto flex flex-col gap-2 max-2xl:hidden">
+      <div className="flex justify-between text-white">
+        <span>Contact</span>
+        <div className="flex gap-2">
+          <SearchIcon />
+          ...
+        </div>
+      </div>
+      <MenuItem
+        icon={Avatar}
+        text="CC20"
+        className="w-11 h-11 rounded-full bg-slate-200"
+        imgSrc='https://www.svgrepo.com/show/303599/spider-man-4-logo.svg'
+      />
+      <MenuItem
+        icon={Avatar}
+        text="CC21"
+        className="w-11 h-11 rounded-full bg-slate-200"
+        imgSrc='https://www.svgrepo.com/show/107388/iron-man.svg'
+      />
+      <MenuItem
+        icon={Avatar}
+        text="CC22"
+        className="w-11 h-11 rounded-full bg-slate-200"
+        imgSrc='https://www.svgrepo.com/show/163518/catwoman.svg'
+      />
+    </div>
+  );
 }
-export default SidebarContact
+export default SidebarContact;
--- src/utils/isTokenExpired.js
+++ src/utils/isTokenExpired.js
@@ -7,4 +7,3 @@ export default function isTokenExpired(token) {
     return true; // invalid token
   }
 }
-∏
