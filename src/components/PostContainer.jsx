4571f4755261cc57207d95c708340bc423e88ba1
armmani <arm.maniii@gmail.com>
2025-07-01 14:27:25 +0700

    npm i react-timeago

--- src/components/PostContainer.jsx
+++ src/components/PostContainer.jsx
@@ -7,24 +7,33 @@ import useUserStore from "../stores/userStore";
 function PostContainer() {
   const posts = usePostStore((state) => state.posts);
   const getAllPosts = usePostStore((state) => state.getAllPosts);
-  const token = useUserStore((state) => state.token);
+  // const token = useUserStore((state) => state.token);
   const [loading, setLoading] = useState(true);
   useEffect(() => {
     const run = async () => {
       setLoading(true);
-      await getAllPosts(token);
+      // await getAllPosts(token);
+      await getAllPosts();
       setLoading(false);
     };
     run();
   }, []);
-  if(loading) {
-    return <span className="loading loading-ball loading-xl"></span>
-  }
+  // if (loading) {
+  //   return <span className="loading loading-ball loading-xl"></span>;
+  // }
 
   return (
     <div className="w-[650px] mx-auto min-h-screen my-3 flex flex-col gap-4 rounded-lg">
       <CreatePost />
-      <pre>{JSON.stringify(posts, null, 2)}</pre>
+      {loading ? (
+        <span className="loading loading-bars loading-xl"></span>
+      ) : (
+        <>
+          {posts.map((el) => (
+            <PostItem key={el.id} post={el} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,16 +1,22 @@
 import { CloseIcon, LikeIcon, ThreeDotIcon } from "../icons";
 import Avatar from "./Avatar";
-import reactLogo from '../assets/react.svg'
+import reactLogo from "../assets/react.svg";
+import useUserStore from "../stores/userStore";
+import usePostStore from "../stores/postStore";
 
-function PostItem() {
+function PostItem({ post }) {
+  // console.log(post)
+  const user = useUserStore((state) => state.user);
+  const token = useUserStore((state) => state.token);
+  const getAllPosts = usePostStore((state) => state.getAllPosts);
   return (
     <div className="card bg-base-100 shadow-xl">
       <div className="card-body">
         <div className="flex justify-between">
           <div className="flex gap-3">
-            <Avatar className="w-11 rounded-full" />
+            <Avatar className="w-11 rounded-full" imgSrc={post.user.profileImage} />
             <div className="flex flex-col">
-              <p>Someone BieSukit</p>
+              <p>{post.user.firstName} {post.user.lastName}</p>
               <p className="text-xs opacity-70">1 day ago</p>
             </div>
           </div>
@@ -23,16 +29,25 @@ function PostItem() {
                   </div>
                 </div>
               </div>
-              <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-                <li> <a>Edit</a></li>
-                <li> <a>Delete</a></li>
+              <ul
+                tabIndex={0}
+                className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+              >
+                <li>
+                  {" "}
+                  <a>Edit</a>
+                </li>
+                <li>
+                  {" "}
+                  <a>Delete</a>
+                </li>
               </ul>
             </div>
             <div className="avatar items-center cursor-pointer">
-                  <div className="w-10 h-10 rounded-full !flex justify-center items-center hover:bg-gray-700">
-                    <CloseIcon className="w-6" />
-                  </div>
-                  </div>
+              <div className="w-10 h-10 rounded-full !flex justify-center items-center hover:bg-gray-700">
+                <CloseIcon className="w-6" />
+              </div>
+            </div>
           </div>
         </div>
         <p>I Love U</p>
@@ -50,14 +65,24 @@ function PostItem() {
           </div>
         </div>
         <div className="divider h-0 my-0"></div>
-         {/* LIKE COMMENT SHARE */}
-         <div className="flex gap-3 justify-between">
-          <div className={`flex gap-3 justify-center items-center cursor-pointer rounded-lg py-2 flex-1 hover:bg-gray-700 ${Math.random()>0.5 ? 'bg-blue-400' : ''}`}>Like</div>
-          <div className="flex gap-3 justify-center items-center cursor-pointer rounded-lg py-2 flex-1 hover:bg-gray-700">Comment</div>
-          <div className="flex gap-3 justify-center items-center cursor-pointer rounded-lg py-2 flex-1 hover:bg-gray-700">Share</div>
-         </div>
-         <div className="divider h-0 my-0"></div>
-         {/* Comment Container */}
+        {/* LIKE COMMENT SHARE */}
+        <div className="flex gap-3 justify-between">
+          <div
+            className={`flex gap-3 justify-center items-center cursor-pointer rounded-lg py-2 flex-1 hover:bg-gray-700 ${
+              Math.random() > 0.5 ? "bg-blue-400" : ""
+            }`}
+          >
+            Like
+          </div>
+          <div className="flex gap-3 justify-center items-center cursor-pointer rounded-lg py-2 flex-1 hover:bg-gray-700">
+            Comment
+          </div>
+          <div className="flex gap-3 justify-center items-center cursor-pointer rounded-lg py-2 flex-1 hover:bg-gray-700">
+            Share
+          </div>
+        </div>
+        <div className="divider h-0 my-0"></div>
+        {/* Comment Container */}
       </div>
     </div>
   );
